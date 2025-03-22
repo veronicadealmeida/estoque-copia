69679d31b9e1ef45b1b29fc0a03c955af4a10576
almeida.veronica <vedev404@gmail.com>
2025-03-22 01:19:22 -0300

    novo build

--- src/main/java/edu/vedev/gestaodeestoque/produto/WebConfig.java
+++ src/main/java/edu/vedev/gestaodeestoque/produto/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081") // Permite CORS de localhost:3000
+                .allowedOrigins("*") // Permite CORS de localhost:3000
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
                 .allowedHeaders("*") // Permite todos os cabeçalhos
                 .allowCredentials(true); // Permite enviar cookies e credenciais
