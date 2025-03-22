9cbdcb8aa065f40c68ab7c2190f0f6c0d5a351d4
almeida.veronica <vedev404@gmail.com>
2025-03-22 00:52:16 -0300

    cria pacore para deploy

--- src/test/java/edu/vedev/gestaodeestoque/GestaodeestoqueApplicationTests.java
+++ src/test/java/edu/vedev/gestaodeestoque/GestaodeestoqueApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GestaodeestoqueApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
