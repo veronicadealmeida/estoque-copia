741c2cb39de38e10ce8d3d8a422482b46ad46c2e
almeida.veronica <vedev404@gmail.com>
2025-03-22 01:12:17 -0300

    cria arquivo de CORS

---
+++ src/main/java/edu/vedev/gestaodeestoque/produto/WebConfig.java
@@ -0,0 +1,19 @@
+package edu.vedev.gestaodeestoque.produto;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8081") // Permite CORS de localhost:3000
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedHeaders("*") // Permite todos os cabeçalhos
+                .allowCredentials(true); // Permite enviar cookies e credenciais
+    }
+}
