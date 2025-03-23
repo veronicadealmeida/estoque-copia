4c713f5f0b57b8d3772a1ae709cca1a8ab9f0fe3
almeida.veronica <vedev404@gmail.com>
2025-03-22 23:30:25 -0300

    cria arquivo de CORS

--- src/main/java/edu/vedev/gestaodeestoque/CorsConfiguration.java
+++ src/main/java/edu/vedev/gestaodeestoque/CorsConfiguration.java
@@ -5,12 +5,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration implements WebMvcConfigurer {
+public @interface CorsConfiguration {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-    }
 }
--- src/main/java/edu/vedev/gestaodeestoque/EstoqueApplication.java
+++ src/main/java/edu/vedev/gestaodeestoque/EstoqueApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-
+@CorsConfiguration
 public class EstoqueApplication {
 
 	public static void main(String[] args) {
@@ -15,13 +15,13 @@ public class EstoqueApplication {
 	}
 
     @Configuration
-    public static class CorsConfiguration implements WebMvcConfigurer {
+    public class CorsConfiguration implements WebMvcConfigurer {
 
         @Override
         public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/api/**")
-                    .allowedOrigins("http://localhost:3000")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT");
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
         }
     }
 }
